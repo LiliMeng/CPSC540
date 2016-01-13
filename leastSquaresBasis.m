be6ae1ab1f65d11870e6a44e3d3c47763c0ca208
LiliMeng <lilimeng1103@gmail.com>
2016-01-12 20:18:10 -0800

    Update leastSquaresBasis.m

--- leastSquaresBasis.m
+++ leastSquaresBasis.m
@@ -3,26 +3,26 @@ function [model] = leastSquaresBasis(X,y,deg)
 n=size(X,1);
 for i=[1:n]
     for j=[1:deg]
-        tmp(i,j)=X(i)^j;
+        X(i,j)=X(i)^j;
     end
 end 
 
-X=[ones(size(X,1),1),tmp]
+X=[ones(size(X,1),1),X];
 w = (X'*X)\X'*y;
 model.w = w;
 model.predict = @predict;
 
 end
 
-function [yhat] = predict(model,Xhat)
+function [yhat] = predict(model,Xhat,deg)
 n=size(Xhat,1);
 for i=[1:n]
     for j=[1:deg]
-        tmp(i,j)=Xhat(i)^j;
+        Xhat(i,j)=Xhat(i)^j;
     end
 end 
 
-Xhat=[ones(size(Xhat,1),1),tmp]
+Xhat=[ones(size(Xhat,1),1),Xhat];
 w = model.w;
 yhat = Xhat*w;
 end
